f1f239453b09d1f933a569cff8e7ce15367a9044
shrasht971 <shrashtisahu5@gmail.com>
2024-04-14 23:30:02 +0530

    Add on question number 1 using while loop

---
+++ whileLoop.java
@@ -0,0 +1,12 @@
+// Write the program print the number 1 to 10 using while loop
+public class whileLoop {
+
+    public static void main(String[] args) {
+       int value = 0;
+       while(value < 10)
+       {
+        System.out.println("hello");
+        value++;
+       }
+    }
+}
