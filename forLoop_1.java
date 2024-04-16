bc03b5a1fe9298fc2e8a3a8e9de4eaf7aab939bf
shrasht971 <shrashtisahu5@gmail.com>
2024-04-17 00:35:18 +0530

    Add on for loop program.

---
+++ forLoop_1.java
@@ -0,0 +1,9 @@
+public class forLoop_1 {
+    public static void main(String[] args) {
+        int i=1;
+        for(;i<=5;i++){
+          System.out.println("Hello" + i);
+        }
+        System.out.println("Hello " +i );
+    }
+}
